0afe03b23272a8eb883822ad29e4e8547bd621da
Rinaldo Medeiros <80865401+rinaldomedeiros@users.noreply.github.com>
2021-04-02 17:58:00 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,230 @@
+var carta1 = {
+    nome: "Seiya de Pégaso",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9a4140b9-d070-4c72-8dc7-7edd2a63a68d/d7z01m5-bd7872d5-00e4-473d-97ea-0c3d155b019a.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvOWE0MTQwYjktZDA3MC00YzcyLThkYzctN2VkZDJhNjNhNjhkXC9kN3owMW01LWJkNzg3MmQ1LTAwZTQtNDczZC05N2VhLTBjM2QxNTViMDE5YS5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.lCzBWkChxdx_-wa1tLkoKCAXNH6iaDt8-sF7UHVeQWo",
+    atributos: {
+        cosmos: 93,
+        ataque: 90,
+        defesa: 90
+    }
+}
+
+var carta2 = {
+    nome: "Shiryu de Dragão",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9a4140b9-d070-4c72-8dc7-7edd2a63a68d/d7z01p9-90b8474a-147b-4dd3-860a-af2021b53a38.jpg/v1/fill/w_1024,h_1487,q_75,strp/shiryu_tenkai_saint_seiya_top_trumps_by_goldmilo_d7z01p9-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xNDg3IiwicGF0aCI6IlwvZlwvOWE0MTQwYjktZDA3MC00YzcyLThkYzctN2VkZDJhNjNhNjhkXC9kN3owMXA5LTkwYjg0NzRhLTE0N2ItNGRkMy04NjBhLWFmMjAyMWI1M2EzOC5qcGciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.NOvDFAolpTBuPNr_bPWPrdGjuvRMHYwERl9zGshs9eg",
+    atributos: {
+        cosmos: 86,
+        ataque: 91,
+        defesa: 96
+    }
+}
+
+var carta3 = {
+    nome: "Shun de Andrômeda",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9a4140b9-d070-4c72-8dc7-7edd2a63a68d/d7z01sa-d0461503-d4ec-4ece-b7c7-f8a8a4a5512b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvOWE0MTQwYjktZDA3MC00YzcyLThkYzctN2VkZDJhNjNhNjhkXC9kN3owMXNhLWQwNDYxNTAzLWQ0ZWMtNGVjZS1iN2M3LWY4YThhNGE1NTEyYi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.samJYX9oBjbDWt7QngeYrLJjw5ajK4_jB0eCphlyVII",
+    atributos: {
+        cosmos: 86,
+        ataque: 89,
+        defesa: 94
+    }
+}
+
+var carta4 = {
+    nome: "Hyoga de Cisne",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9a4140b9-d070-4c72-8dc7-7edd2a63a68d/d7z018y-d140d3e3-482c-427e-a2e0-04927a2034cd.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvOWE0MTQwYjktZDA3MC00YzcyLThkYzctN2VkZDJhNjNhNjhkXC9kN3owMTh5LWQxNDBkM2UzLTQ4MmMtNDI3ZS1hMmUwLTA0OTI3YTIwMzRjZC5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.smflnRe6qqTuuhhtpMn_C7-IyL766SaOxvl_F_EpSzg",
+    atributos: {
+        cosmos: 87,
+        ataque: 90,
+        defesa: 90
+    }
+}
+
+var carta5 = {
+    nome: "Saga de Virgem",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9a4140b9-d070-4c72-8dc7-7edd2a63a68d/d7ax5zx-97bb3341-f338-4223-8caf-bc5b4ac9441b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvOWE0MTQwYjktZDA3MC00YzcyLThkYzctN2VkZDJhNjNhNjhkXC9kN2F4NXp4LTk3YmIzMzQxLWYzMzgtNDIyMy04Y2FmLWJjNWI0YWM5NDQxYi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.lNhx2ZFkqC1QEzIlgZ9Jy7K-YMAwdpRd9npyjLz-6ms",
+    atributos: {
+        cosmos: 73,
+        ataque: 94,
+        defesa: 85
+    }
+}
+
+var carta6 = {
+    nome: "Aiolia de Leão",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9a4140b9-d070-4c72-8dc7-7edd2a63a68d/d7awx4u-1d77caca-3030-4186-8323-5758ad3ab156.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvOWE0MTQwYjktZDA3MC00YzcyLThkYzctN2VkZDJhNjNhNjhkXC9kN2F3eDR1LTFkNzdjYWNhLTMwMzAtNDE4Ni04MzIzLTU3NThhZDNhYjE1Ni5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.dHYUa7d5Yx3Nxn5bdKDIf3HnmqaBXT8bg9klBpUdWDM",
+    atributos: {
+        cosmos: 68,
+        ataque: 90,
+        defesa: 78
+    }
+}
+
+var carta7 = {
+    nome: "Afrodite de Peixes",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9a4140b9-d070-4c72-8dc7-7edd2a63a68d/d7awx7i-aa9354e8-9c60-408c-9b2a-5b65e42fcb1e.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvOWE0MTQwYjktZDA3MC00YzcyLThkYzctN2VkZDJhNjNhNjhkXC9kN2F3eDdpLWFhOTM1NGU4LTljNjAtNDA4Yy05YjJhLTViNjVlNDJmY2IxZS5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.iVew_uIItzm7LvJ_DT7YwbI7hW3olLdvi5weXa0iDX4",
+    atributos: {
+        cosmos: 67,
+        ataque: 94,
+        defesa: 76
+    }
+}
+
+var carta8 = {
+    nome: "Aldebaran de Touro",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9a4140b9-d070-4c72-8dc7-7edd2a63a68d/d7awx5o-32c0f74b-6134-4303-94da-29f6e6ff1fc6.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvOWE0MTQwYjktZDA3MC00YzcyLThkYzctN2VkZDJhNjNhNjhkXC9kN2F3eDVvLTMyYzBmNzRiLTYxMzQtNDMwMy05NGRhLTI5ZjZlNmZmMWZjNi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.s5k2lBooYceLSuhVJ-CaxknnJRx7L6jKadzilc7K6Mk",
+    atributos: {
+        cosmos: 69,
+        ataque: 91,
+        defesa: 81
+    }
+}
+
+var cartaMaquina
+var cartaJogador
+var cartas = [carta1,carta2,carta3,carta4,carta5,carta6,carta7,carta8]
+     
+var vezDoJogador = true
+var pontosJogador = 0
+var pontosMaquina = 0
+atualizaPlacar()
+atualizaQuantidadeCartas()
+
+function atualizaQuantidadeCartas(){
+  var divQuantidadeCartas = document.getElementById('quantidade-cartas')
+  var html = "Quantidade de cartas no jogo: " + cartas.length
+  divQuantidadeCartas.innerHTML = html
+}
+
+function atualizaPlacar(){
+  var divPlacar = document.getElementById('placar')
+  var html = "Jogador " + pontosJogador + " x " + pontosMaquina + " Máquina"
+  divPlacar.innerHTML = html
+}
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
+    cartaMaquina = cartas[numeroCartaMaquina]
+    cartas.splice(numeroCartaMaquina,1)
+  
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+    cartaJogador = cartas[numeroCartaJogador]
+    cartas.splice(numeroCartaJogador,1)
+  
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+    
+    exibeCartaJogador()
+    verificaVezDeJogar()
+}
+
+function exibeCartaJogador() {
+    var divCartaJogador = document.getElementById("carta-jogador")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status'>"
+
+    divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value
+        }
+    }
+}
+
+function verificaVezDeJogar(){
+    var radioAtributo = document.getElementsByName('atributo')
+    if(vezDoJogador){
+      alert("Sua vez de escolher um atributo")
+      radioAtributo.disabled = false
+      vezDoJogador = false      
+    }else{
+      alert("A máquina escolheu um atributo.")
+      var escolhaMaquina = parseInt(Math.random() * 3)
+      desabilitaAtributo(radioAtributo)
+      radioAtributo[escolhaMaquina].checked = true
+      radioAtributo[escolhaMaquina].set
+      vezDoJogador = true
+    }
+}
+
+function desabilitaAtributo(radioAtributo){
+  for(var i=0; i< 3; i++){
+    radioAtributo[i].disabled = true
+  } 
+}
+
+function jogar() {
+    var divResultado = document.getElementById("resultado")
+    var atributoSelecionado = obtemAtributoSelecionado()
+
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Venceu</p>'
+        pontosJogador++
+    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Perdeu</p>'
+        pontosMaquina++
+    } else {
+        htmlResultado = '<p class="resultado-final">Empatou</p>'
+    }
+ 
+    divResultado.innerHTML = htmlResultado
+    exibeCartaMaquina()
+    atualizaPlacar()
+    document.getElementById('btnJogar').disabled = true
+    document.getElementById('btnProximaRodada').disabled = false
+    
+    atualizaQuantidadeCartas()
+  
+    if(cartas.length == 0){
+      alert("Fim de jogo")
+      if(pontosJogador > pontosMaquina){
+        htmlResultado = '<p class="resultado-final">VocÊ Venceu</p>'
+      }else if(pontosJogador < pontosMaquina){
+        htmlResultado = '<p class="resultado-final">VocÊ Perdeu</p>'     
+      }else{
+        htmlResultado = '<p class="resultado-final">Empatou</p>'
+      }
+      divResultado.innerHTML = htmlResultado
+      document.getElementById('btnProximaRodada').disabled = true
+    }
+}
+
+function exibeCartaMaquina() {
+    var divCartaMaquina = document.getElementById("carta-maquina")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaMaquina.atributos) {
+        opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status --spacing'>"
+
+    divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function proximaRodada(){
+  var divCartas = document.getElementById('cartas')
+  divCartas.innerHTML = `<div id="carta-jogador" class="carta"></div> <div id="carta-maquina" class="carta"></div>`
+  
+  document.getElementById('btnSortear').disabled = false
+  document.getElementById('btnJogar').disabled = true
+  document.getElementById('btnProximaRodada').disabled = true
+  
+  var divResultado = document.getElementById('resultado')
+  divResultado.innerHTML = ""
+  atualizaQuantidadeCartas()
+  
+}
